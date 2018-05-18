b5b1241e4f1df6b5ebb6c741a190baef40845d3e
Ranaivoson James <ranaivosonjames@yahoo.fr>
2018-05-18 10:28:29 +0200

    ajout paragraphe + css

--- index.php
+++ index.php
@@ -13,10 +13,19 @@
                     font-family : Calibri;
                     font-size : 40px;
                 }
+                i {
+                    position : absolute;
+                    top : 52%;
+                    left : 32%;
+                    font-family : Calibri;
+                    font-size : 40px;
+                    color : black;
+                }
          
          </style>
     </head>
     <body>
         <p>Hello !!!</p>
+        <i>On est vendredi et c'est bientot le week end</i>
     </body>
 </html>
