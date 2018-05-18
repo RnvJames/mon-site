a8d03320e48ca3c3bd0daefe5b8e3ad2121dee13
Ranaivoson James <ranaivosonjames@yahoo.fr>
2018-05-18 10:03:58 +0200

    ajout css

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
     <head>
         <title>Mon SITE</title>
         <meta charset "utf-8">
+         <link rel="stylesheet" href="style.css">
+         <style>
+                body {background : grey;}
+                p {
+                    position : absolute;
+                    top : 40%;
+                    left : 40%;
+                    font-family : Calibri;
+                    font-size : 40px;
+                }
+         
+         </style>
     </head>
     <body>
         <p>Hello !!!</p>
